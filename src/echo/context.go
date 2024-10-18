19ed178cb20db048b6b7c011b5be414d6cc2fe94
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2024-10-18 14:08:12 +0700

    📦 added admin only middleware

--- src/echo/context.go
+++ src/echo/context.go
@@ -1,6 +1,7 @@
 package echo
 
 import (
+	"net/http"
 	"github.com/UnknownRori/lagra_server/src/models"
 
 	"github.com/charmbracelet/log"
@@ -31,6 +32,25 @@ func AuthenticatedContextMiddleware(s *Server) echo.MiddlewareFunc {
 				Context: c,
 				User:    userModel,
 			})
+		}
+	}
+}
+
+func AdminContextMiddleware(s *Server) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			cc := c.(*AuthenticatedContext)
+			user := cc.User
+
+			if user.Role != "ADMIN" {
+				log.Fatal("Invalid access")
+				return c.JSON(http.StatusNotFound, echo.Map{
+					"message": "Data not found!",
+					"status":  "fail",
+				})
+			}
+
+			return next(cc)
 		}
 	}
 }
